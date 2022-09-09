2b7f078f3afdbd9ff0f3300466fa00cca598582f
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-09-08 22:52:12 -0600

    Create principal UI for Calculate Sets

--- django_sets/urls.py
+++ django_sets/urls.py
@@ -20,5 +20,6 @@ from solve_sets import views
 
 urlpatterns = [
     #[path("admin/", admin.site.urls)]
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('sets/', views.sets, name='sets')
 ]
--- solve_sets/views.py
+++ solve_sets/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
    return render(request, 'home.html')
+
+def sets(request):
+   operation = request.GET.get('sets')
+   return render(request, 'sets.html', {'set_solved': operation})
