2d0f4acef89b98d11866e740fef5013d50d7d169
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-09-16 22:12:34 -0600

    Sets to list

--- solve_sets/views.py
+++ solve_sets/views.py
@@ -9,7 +9,11 @@ def sets(request):
    return render(request, 'sets.html', {'set_solved': sets})
 
 def solve_sets(sets:str):
-   pass
+   sets = format_sets(sets)
+   print(sets_to_lists(sets))
 
-def format_sets():
+def format_sets(sets:str) -> str:
    return sets.replace(" ", "")
+
+def sets_to_lists(sets:str) -> list:
+   return [ str(set) for set in sets.split()]
