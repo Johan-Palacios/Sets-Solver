84fef917dd4c7fb05b2a551e341468d3bb1969f8
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-09-21 22:00:18 -0600

    Add editor config

--- solve_sets/views.py
+++ solve_sets/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-   return render(request, 'components/_setform.html')
+    return render(request, 'components/_setform.html')
+
 
 def sets(request):
-   sets = request.GET.get('sets')
-   solve_sets(str(sets))
-   return render(request, 'sets.html', {'set_solved': sets})
+    sets = request.GET.get('sets')
+    solve_sets(str(sets))
+    return render(request, 'sets.html', {'set_solved': sets})
 
-def solve_sets(sets:str):
-   sets = format_sets(sets)
-   print(sets_to_lists(sets))
+def solve_sets(sets: str):
+    sets = format_sets(sets)
+    print(sets_to_lists(sets))
 
-def format_sets(sets:str) -> str:
-   return sets.replace(" ", "")
+def format_sets(sets: str) -> str:
+    return sets.replace(" ", "")
 
-def sets_to_lists(sets:str) -> list:
-   return [ str(set) for set in sets.split()]
+def sets_to_lists(sets: str) -> list:
+    return [str(set) for set in sets.split()]
