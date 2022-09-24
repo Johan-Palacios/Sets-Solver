81d1b577b3a2bb5051a0e03c5064b2fa9b83680f
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-09-23 23:07:30 -0600

    Validate Correct sets

--- solve_sets/views.py
+++ solve_sets/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
+import re
+
 
 def home(request):
-    return render(request, 'components/_setform.html')
+    return render(request, "components/_setform.html")
+
 
 def sets(request):
-    sets = request.GET.get('sets')
-    solve_sets(str(sets))
-    return render(request, 'sets.html', {'set_solved': sets})
+    sets = request.GET.get("sets")
+    operation = request.GET.get("sets_operation")
+    solve_sets(str(sets), str(operation))
+    return render(request, "sets.html", {"set_solved": sets})
 
-def solve_sets(sets: str):
+
+def solve_sets(sets: str, operation: str):
     sets = format_sets(sets)
-    print(sets_to_lists(sets))
+    operation = format_sets(operation)
+    sets = item_to_lists(sets)
+    operation = item_to_lists(operation)
+    print(validate_operation(operation))
+
 
 def format_sets(sets: str) -> str:
     return sets.replace(" ", "")
 
-def sets_to_lists(sets: str) -> list:
+
+def item_to_lists(sets: str) -> list:
     return [str(set) for set in sets.split()]
+
+
+def validate_sets(sets: list):
+    valid_sets = []
+    for set in sets:
+        if matches := re.search(r"^([A-TV-Z])=\{(.+)+\}$", set):
+            valid_sets.append(set)
+    return valid_sets
+
+
+def validate_operation(operations: list) -> list:
+    valid_operations = []
+    for operation in operations:
+        if matches := re.search(r"^([A-TV-Z\-|&\(\)])+$", operation):
+            valid_operations.append(operation)
+    return valid_operations
