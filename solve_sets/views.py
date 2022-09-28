12bbd7e1250cc9fe33448a12288816b3c94626e4
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-09-27 23:26:16 -0600

    Create sets, and operate betwen of them

--- solve_sets/views.py
+++ solve_sets/views.py
@@ -20,15 +20,15 @@ def solve_sets(sets: str, operation: str):
     list_operations = item_to_lists(operation)
     list_sets = validate_sets(list_sets)
     list_operations = validate_operation(list_operations)
-    operate_set(list_sets)
+    operate_set(list_sets, list_operations)
 
 
 def format_sets(sets: str) -> str:
     return sets.replace(" ", "")
 
 
-def item_to_lists(sets: str) -> list:
-    return [str(set) for set in sets.split()]
+def item_to_lists(sets: str, param=None) -> list:
+    return [str(set) for set in sets.split(param)]
 
 
 def append_set(elements: str) -> set:
@@ -43,9 +43,7 @@ def validate_sets(sets: list):
                 valid_sets.append(
                     {
                         "setName": matches.group(1),
-                        "setValue": [
-                            str(value) for value in matches.group(2).split(",")
-                        ],
+                        "setValue": item_to_lists(matches.group(2), ","),
                     }
                 )
             except:
@@ -61,8 +59,25 @@ def validate_operation(operations: list) -> list:
     return valid_operations
 
 
-def operate_set(sets: list):
+def operate_set(sets: list, operations: list):
+    operable_sets = []
+    correct_operations = []
     for set in sets:
         set_name = str(set["setName"])
         set_value = append_set(set["setValue"])
-        print(set_value)
+        try:
+            operable_sets.append({"set": exec(f"{set_name} = set_value")})
+        except:
+            pass
+    for operation in operations:
+        try:
+            correct_operations.append(
+                {"operation": operation, "resut": exec(f"print({operation})")}
+            )
+        except:
+            correct_operations.append(
+                {
+                    "operation": operation,
+                    "resut": "Error al operar, verifique su operación",
+                }
+            )
