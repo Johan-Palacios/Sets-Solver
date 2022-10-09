059ecf4b23b3ccd21befede500cdd5b0855fbd0d
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-10-09 00:15:49 -0600

    Security provided by Envorion 🔐

--- django_sets/settings.py
+++ django_sets/settings.py
@@ -29,7 +29,7 @@ env.read_env()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
