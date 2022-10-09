823262e19aa6b8554fe851d27f9efcf46be566aa
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-10-08 22:18:17 -0600

    Create auto setup

---
+++ create/make.py
@@ -0,0 +1,19 @@
+import os
+import platform
+
+def create_linux_env():
+    try:
+        os.system('python3.10 -m venv venv')
+        os.system('source ./venv/bin/activate')
+        os.system('python3.10 -m pip install -r requirements.txt')
+        print("Ejecute python3.10 manage.py runserver")
+    except Exception as e:
+        print("Ha ocurrido un error")
+        raise e
+
+if platform.system() in ["Linux", "Darwin"]:
+    create_linux_env()
+elif platform.system() == "Windows":
+    pass
+else:
+    print("Plataforma No soportada")
