d895e83869a823cf6c1e4cb8cc04bd9cf290ef72
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-10-08 22:45:14 -0600

    Experiment Process

--- create/make.py
+++ create/make.py
@@ -1,11 +1,13 @@
 import os
 import platform
+import subprocess
 
 def create_linux_env():
     try:
         os.system('python3.10 -m venv venv')
         os.system('source ./venv/bin/activate')
-        os.system('python3.10 -m pip install -r requirements.txt')
+        # os.system('python3.10 -m pip install -r requirements.txt')
+        subprocess.call("command", shell=True, executable='/bin/bash')
         print("Ejecute python3.10 manage.py runserver")
     except Exception as e:
         print("Ha ocurrido un error")
