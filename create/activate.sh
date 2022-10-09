94f0834d2cf7e5b8957d3c0c847f06bc53fc70bb
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-10-08 23:55:53 -0600

    Make.py

--- create/activate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ../venn/bin/activate
-exit
--- create/make.py
+++ create/make.py
@@ -1,19 +1,64 @@
 import os
 import platform
-import subprocess
 
-def create_linux_env():
+
+option = int(input("1.Crear entorno virtual\n2.Instalar dependencias\n--> "))
+
+
+def create_env(command: str):
     try:
-        os.system('python3.10 -m venv venv')
-        os.system('create/activate.sh')
-        print("Ejecute python3.10 manage.py runserver")
+        print("Creando entorno virtual")
+        os.system(command)
+        print("Ejecute source ./venv/bin/activate o venv\\Scripts\\activate.bat(Windows)")
     except Exception as e:
         print("Ha ocurrido un error")
         raise e
 
-if platform.system() in ["Linux", "Darwin"]:
-    create_linux_env()
-elif platform.system() == "Windows":
-    pass
-else:
-    print("Plataforma No soportada")
+
+def install_dependences(command: str):
+    try:
+        print("Instalando dependencias")
+        os.system("")
+    except Exception as e:
+        print("No es posible instalar las dependencias")
+        raise e
+
+
+def create_windows_env():
+    try:
+        print("Creando entorno virtual")
+        os.system("python.exe -m venv venv")
+        print("Ejecute venv\\Scripts\\activate.bat")
+    except Exception as e:
+        print("Ha ocurrido un error")
+        raise e
+
+
+def set_command(venv: str, install: str):
+    if option == 1:
+        create_env(venv)
+    elif option == 2:
+        install_dependences(install)
+    else:
+        invalid()
+
+
+def invalid():
+    print("Invalid option")
+
+
+def build():
+    if platform.system() in ["Linux", "Darwin"]:
+        set_command(
+            "python3 -m venv venv", "python3 -m pip install -r requirements.txt"
+        )
+    elif platform.system() == "Windows":
+        set_command(
+            "python.exe -m venv venv", "python.exe -m pip install -r requirements.txt"
+        )
+    else:
+        print("Plataforma No soportada")
+
+
+if __name__ == "__main__":
+    build()
